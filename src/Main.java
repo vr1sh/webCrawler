6aa315e3c5f2714a6e4a3743dba7c394aaae842f
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-06-16 12:25:32 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,24 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import javax.print.Doc;
+import java.util.Scanner;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
+        System.out.println("Enter URL: ");
+        Scanner scan = new Scanner(System.in);
+        String url = scan.nextLine();
+
         System.setProperty("http.proxyhost", "127.0.0.1");
         System.setProperty("http.proxyport", "3128");
 
-        Document document = Jsoup.connect("https://medium.com").
+        Document document = Jsoup.connect(url).
                 userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36")
-                .ignoreContentType(true).timeout(1000).get();
+                .ignoreContentType(true).timeout(5000).get();
         String title = document.title();
         System.out.println();
         System.out.println("Website Title: " + title);
